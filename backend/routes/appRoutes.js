1e1f03756b8bda3f8c944d17e7c6f0c0bbf28e4e
snlaight <snlaight10@gmail.com>
2021-12-10 11:07:02 +0100

    code readability for backend routes

--- backend/routes/appRoutes.js
+++ backend/routes/appRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const urlModel = require("../models/url.model");
-const userModel = require("../models/user.model");
+
+const Services = require("./helpers/Service")
+
+const users = Services.getAllUsers;
+const urls = Services.getAllUrls;
+const shortUrlRedirect = Services.getShortUrlAndRedirect;
 
 //home route of the app to render data, still TBC
 router.get("/", (req, res) => {
@@ -10,52 +14,13 @@ router.get("/", (req, res) => {
 
 
 //simple route to display all of the urls currently in the DB (for testing with Postman)
-router.get("/urls", async (req, res) => {
-  let urls = await urlModel
-    .find()
-    .then((allUrls) => {
-      return allUrls;
-    })
-    .catch((e) => {
-      console.log(`we have the following error: ${e}`);
-    });
-  res.send(urls);
-});
+router.get("/urls", urls);
 
 //simple route to display all of the users currently in the DB (for testing with postman)
-router.get("/users", async (req, res) => {
-  let users = await userModel
-    .find()
-    .then((allUsers) => {
-      return allUsers;
-    })
-    .catch((e) => {
-      console.log(`we have the following error: ${e}`);
-    });
-  res.send(users);
-});
+router.get("/users", users);
 
 
 //this route grabs the shortlURl via the params of the query and redirects to the long/original URL stored in the database.
-router.get("/SUA/:shorturl", async (req, res) => {
-  const shorturl = req.params.shorturl;
-  const findUrl = await urlModel.findOne({
-    shortUrl: shorturl,
-  });
-  if (findUrl) {
-    let timesClicked = findUrl.timesClicked;
-    timesClicked++;
-    await findUrl.updateOne({ timesClicked: timesClicked });
-    res.redirect(findUrl.longUrl);
-  }
-  if (!findUrl) {
-    res.send({
-      status: 400,
-      message: `Could not find ${shorturl} in database.`,
-    });
-  }
-
-  return findUrl;
-});
+router.get("/SUA/:shorturl", shortUrlRedirect);
 
 module.exports = router;
--- backend/routes/helpers/Service.js
+++ backend/routes/helpers/Service.js
@@ -5,43 +5,33 @@ module.exports = {
   async checkForExistingUrl(req, res) {
     let originalUrl = req.body.url;
     try {
-      let checkForUrl = await urlModel
-        .findOne({ longUrl: originalUrl })
-        .then((foundUrl) => {
-          return foundUrl;
-        });
+      let checkForUrl = await urlModel.findOne({ longUrl: originalUrl });
       if (checkForUrl) {
         res.send({
           status: 409,
           message: `Url already exists in DB as ${checkForUrl.shortUrl}`,
           shortUrl: `${checkForUrl.shortUrl}`,
           redirectUrl: `http://localhost:3050/SUA/${checkForUrl.shortUrl}`,
-          longUrl: `${checkForUrl.originalUrl}`,
+          longUrl: `${checkForUrl.longUrl}`,
         });
       } else {
-        createUrl(originalUrl)
+        let shortUrl = Math.random().toString(36).substring(9, 15);
+        newUrl = new urlModel({
+          longUrl: originalUrl,
+          shortUrl,
+          timesClicked: 0,
+        });
+        await newUrl.save();
+        res.send({
+          status: 200,
+          message: `URL succesfully created and stored as ${shortUrl} `,
+          shortUrl: `${shortUrl}`,
+          redirectUrl: `http://localhost:3050/SUA/${shortUrl}`,
+          longUrl: `${originalUrl}`,
+        });
       }
     } catch (e) {
-      res.send({ status: 400, message: e });
-    }
-  },
-
-  async createUrl(originalUrl, res) {
-    let shortUrl = Math.random().toString(36).substring(9, 15);
-    try {
-      await urlModel
-        .create({ longUrl: originalUrl, shortUrl, timesClicked: 0 })
-        .then(
-          res.send({
-            status: 200,
-            message: `URL succesfully created and stored as ${shortUrl} `,
-            shortUrl: `${shortUrl}`,
-            redirectUrl: `http://localhost:3050/SUA/${shortUrl}`,
-            longUrl: `${originalUrl}`,
-          })
-        );
-    } catch (e) {
-      res.send({ status: 400, message: e });
+      res.send({ status: 400, message: `We have the following error: ${e}` });
     }
   },
 
@@ -70,29 +60,60 @@ module.exports = {
           status: 409,
           message: `User with ${email} already exists. Please provide another email`,
         });
-      } else {
-        createUser(email).then((res) =>
-          res.send({
-            status: 200,
-            message: `New user with email ${email} succesfully created.`,
-          })
-        );
       }
+      newUser = new userModel(req.body);
+      await newUser.save();
+      res.send({
+        status: 200,
+        message: `User with email ${email} succesfully created.`,
+      });
+      return;
     } catch (e) {
-      res.send({ status: 400, message: e });
+      res.send({ status: 400, message: `we have the following error: ${e}` });
     }
   },
 
-  async createUser(email, res) {
+  async getAllUrls(req, res) {
     try {
-      await userModel.create({ email }).then((newUser) =>
-        res.send({
-          status: 200,
-          message: `New user with email ${email} succesfully created.`,
-        })
-      );
+      let urls = await urlMode.find().then((allUrls) => {
+        return allUrls;
+      });
+      res.send(urls);
     } catch (e) {
-      res.send({ status: 400, message: e });
+      res.send({ status: 500, message: `we have the following error : ${e}` });
+    }
+  },
+
+  async getAllUsers(req, res) {
+    try {
+      let users = await userModel.find().then((allUsers) => {
+        return allUsers;
+      });
+      res.send(users);
+    } catch (e) {
+      res.send({ status: 500, message: `we have the following error : ${e}` });
+    }
+  },
+  async getShortUrlAndRedirect(req, res) {
+    const shorturl = req.params.shorturl;
+    try {
+      const findUrl = await urlModel.findOne({
+        shortUrl: shorturl,
+      });
+
+      let timesClicked = findUrl.timesClicked++;
+      if (findUrl) {
+        timesClicked++;
+        await findUrl.updateOne({ timesClicked: timesClicked });
+        res.redirect(findUrl.longUrl);
+      } else {
+        res.send({
+          status: 400,
+          message: `Could not find ${shorturl} in database.`,
+        });
+      }
+    } catch (e) {
+      res.send({ status: 500, message: `we have the following error : ${e}` });
     }
   },
 };
